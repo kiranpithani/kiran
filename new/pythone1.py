7662ce9125b2f828932720528f25788a537b4d19
kiranpithani <kiranpithani1.kp@gmail.com>
2022-01-06 16:42:38 +0530

    6th newly adding commit

---
+++ new/pythone1.py
@@ -0,0 +1,9 @@
+number is given on a separate line.
+
+length = int(input("enter the length of the base of the triangle: "))
+height = int(input("enter the length of height of the triangle: "))
+
+area=(length*height)//2
+#the floor division // rounds the reasult down to the nearest whole number
+
+print(f"the area of the triangle is (area)")
